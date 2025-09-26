2ec4cbccbf935fc6350647aa9df7004810ff6c14
aansalov <aansalov@avito.ru>
2025-09-26 16:47:10 +0300

    Add Search window

---
+++ SearchScreen.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  Image,
+  StatusBar,
+} from 'react-native';
+
+const SearchScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor="#F5A8D4" />
+
+      {/* Background image */}
+      <Image
+        source={require('../Diary/Parts/Background.png')}
+        style={styles.backgroundImage}
+        resizeMode="cover"
+      />
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5A8D4',
+  },
+  backgroundImage: {
+    position: 'absolute',
+    width: 1248,
+    height: 2700,
+    top: 0,
+    left: -3,
+    transform: [{ rotate: '0deg' }],
+    opacity: 1,
+  },
+});
+
+export default SearchScreen;
